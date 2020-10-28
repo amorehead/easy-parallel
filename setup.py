e9197cd5d439c81324aace9457337801cbf450e6
Alex Morehead <alex.morehead@gmail.com>
2020-10-27 23:35:37 -0500

    Update PyPI module name

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='easy-parallel-py3',
-    py_modules=['parallel-py3'],
+    py_modules=['parallel'],
     url='https://github.com/amorehead/easy-parallel',
-    version='0.1.6.3',
+    version='0.1.6.4',
     description='Parallel wrapper for easy multi-threading (Python 3 version).',
     long_description=open("README.rst").read(),
     author='Raphael Townshend',
